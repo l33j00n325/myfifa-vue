9672151374ba40a6cff0c25beb8c5ebcef04e813
Joon Lee <l33j00n325@gmail.com>
2020-11-17 14:55:48 -0800

    add TeamCalendar to display Matches

--- components/Team/TeamCalendar.vue
+++ components/Team/TeamCalendar.vue
@@ -1,78 +1,74 @@
 <template>
-  <v-row>
-    <v-col
-      class="text-left"
-      cols="3"
-    >
+  <v-card>
+    <v-toolbar flat>
+      <v-btn
+        outlined
+        class="mr-4"
+        color="grey darken-2"
+        @click="day = team.currently_on"
+      >
+        Today
+      </v-btn>
       <v-btn
         icon
+        text
+        small
+        color="grey darken-2"
         @click="$refs.calendar.prev()"
       >
-        <v-icon>mdi-chevron-left</v-icon>
+        <v-icon small>mdi-chevron-left</v-icon>
       </v-btn>
-    </v-col>
-    <v-col
-      class="text-center"
-      cols="6"
-    >
-      <v-btn text>{{ currentMonth }}</v-btn>
-    </v-col>
-    <v-col
-      class="text-right"
-      cols="3"
-    >
       <v-btn
         icon
+        text
+        small
+        color="grey darken-2"
         @click="$refs.calendar.next()"
       >
-        <v-icon>mdi-chevron-right</v-icon>
+        <v-icon small>mdi-chevron-right</v-icon>
       </v-btn>
-    </v-col>
-    <v-col cols="12">
+      <v-toolbar-title
+        v-if="$refs.calendar"
+        class="ml-4"
+      >
+        {{ $refs.calendar.title }}
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-card-text>
       <v-calendar
         ref="calendar"
         v-model="day"
         type="month"
         :now="team.currently_on"
         :start="team.started_on"
+        :events="events"
+        event-start="played_on"
+        :event-color="getEventColor"
         :show-month-on-first="false"
+        @click:event="viewMatch"
+      />
+      <v-menu
+        v-model="selectedOpen"
+        :close-on-content-click="false"
+        :activator="selectedElement"
+        min-width="300"
+        offset-y
+        offset-overflow
       >
-        <template #day="{ date }">
-          <v-sheet
-            v-for="match in matchesOn(date)"
-            :key="match.id"
-            tile
-            class="pa-1 transparent"
-          >
-            <v-dialog width="500">
-              <template #activator="{ on }">
-                <a
-                  class="d-block text-center"
-                  v-on="on"
-                >
-                  <fitty-text
-                    :text="match.opponent"
-                    :max-size="14"
-                  />
-                </a>
-              </template>
-              <match-card
-                :match="match"
-                :title="`${match.home} v ${match.away}`"
-                color="light-blue"
-              />
-            </v-dialog>
-          </v-sheet>
-        </template>
-      </v-calendar>
-    </v-col>
-  </v-row>
+        <match-card
+          :match="selectedEvent"
+          :title="selectedEvent.opponent"
+          :color="selectedEvent.color"
+        />
+      </v-menu>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
   import { Match } from '@/models'
   import { TeamAccessible } from '@/mixins'
-  import { format, parseISO } from 'date-fns'
 
   export default {
     name: 'TeamCalendar',
@@ -80,24 +76,51 @@
       TeamAccessible
     ],
     data: () => ({
-      day: format(new Date(), 'yyyy-MM-dd')
+      day: new Date(),
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false
     }),
     computed: {
-      currentMonth () {
-        return format(parseISO(this.day), 'MMMM yyyy')
+      matches () {
+        return Match
+          .query()
+          .with('team')
+          .where('team_id', this.team.id)
+          .get()
+      },
+      events () {
+        return this.matches.map(match => ({
+          ...match,
+          name: `${match.score} — ${match.opponent}`,
+          opponent: match.opponent,
+          color: match.resultColor,
+          link: match.link
+        }))
       }
     },
     mounted () {
       this.day = this.team.currently_on
     },
     methods: {
-      matchesOn (date) {
-        return Match
-          .query()
-          .with('team')
-          .where('team_id', this.team.id)
-          .where('played_on', date)
-          .get()
+      viewMatch ({ nativeEvent, event }) {
+        const open = () => {
+          this.selectedEvent = event
+          this.selectedElement = nativeEvent.target
+          setTimeout(() => {
+            this.selectedOpen = true
+          }, 10)
+        }
+
+        if (this.selectedOpen) {
+          this.selectedOpen = false
+          setTimeout(open, 10)
+        } else {
+          open()
+        }
+      },
+      getEventColor (event) {
+        return event.color
       }
     }
   }
--- pages/teams/_teamId/index.vue
+++ pages/teams/_teamId/index.vue
@@ -95,6 +95,10 @@
           />
         </v-col>
       </v-col>
+      <!-- Match Calendar -->
+      <v-col cols="12">
+        <team-calendar />
+      </v-col>
     </v-row>
   </v-container>
 </template>
